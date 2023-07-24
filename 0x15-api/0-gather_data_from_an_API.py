c95a8bff03adddb0482c6e771b367b973345ed77
Mur-tal <muritala.gbadamosi@yahoo.com>
2023-07-24 10:10:43 -0700

    Task #0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,8 @@
-'''
 #!/usr/bin/python3
 """ Python script that uses [REST API](https://jsonplaceholder.typicode.com/),
     for a given employee ID,
     returns information about his/her TODO list progress
 """
-import requests
-import sys
-
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/users"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(i)) for i in completed]
-
-'''
-#!/usr/bin/python3
-"""Script that uses REST API"""
 import requests
 import sys
 
