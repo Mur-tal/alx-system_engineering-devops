e06efea64d17e593c2cd51f4ac1de65ac141a650
Mur-tal <118113403+Mur-tal@users.noreply.github.com>
2023-10-03 14:51:20 +0100

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,7 +0,0 @@
-# Script to change the OS configuration so that it is possible to
-# login with the holberton user and open a file without any error message
-exec { 'Change_OS_config':
-  command  => 'sudo sed -i "s/4/40000/" /etc/security/limits.conf; sudo sed -i "s/5/50000/" /etc/security/limits.conf',
-  provider => shell,
-  returns  => [0, 1]
-}
